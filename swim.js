26d0a5c4ca2a7a42bb32e9a4d32f7a4b0ea10ca5
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2015-11-07 19:17:04 -0800

    animations

--- swim.js
+++ swim.js
@@ -1,6 +1,22 @@
-var fish = document.getElementById('logo');
+var logo = document.getElementById("logo");
+var staticFish = document.getElementById("static");
+var platter = document.getElementById("platter");
+var fish = document.getElementById("fish");
+var about = document.getElementById("about")
 
 
-fish.addEventListener("click", function () {
-  fish.addAttribute("class", "fish");
+logo.addEventListener("click", function () {
+  staticFish.setAttribute("class", "hidden");
+  platter.removeAttribute("class", "hidden");
+  fish.removeAttribute("class", "hidden");
+  fish.setAttribute("class", "fish");
 });
+
+about.addEventListener("click", function(){
+  fish.removeAttribute("class", "fish");
+  fish.setAttribute("class", "hidden");
+  platter.setAttribute("class", "hidden");
+  staticFish.removeAttribute("class", "hidden");
+});
+
+
